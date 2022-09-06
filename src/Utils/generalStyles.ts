5641c7030cd7b589dc0b5dff38360cea4055036c
RaulECT <eduardoct120@gmail.com>
2022-09-06 10:28:55 -0500

    feat: styles utils created

---
+++ src/Utils/generalStyles.ts
@@ -0,0 +1,11 @@
+export const SIZES = {
+  COUNTER_TEXT: '4rem',
+  TITLE_TEXT: '2.1rem'
+};
+
+export const COLORS = {
+  BLACK: '#37323E',
+  GREY: '#BFBDC1',
+  ORANGE: '#DE9E36',
+  WHITE: '#FFFFFF'
+}
